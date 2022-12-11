3140198da2d68e2b96bc47e648b31c28ae695039
Anna Tsepilova <virgo.tsav@gmail.com>
2022-12-11 21:18:49 +0200

    add class LocalStorage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { CocktailsApi } from './js/cocktailsApi';
-import * as localStorage from './js/localStorage';
+import { LocalStorage } from './js/localStorage';
 import * as favorites from './js/favorites';
 import * as themeSwitch from './js/themeSwitch';
 import { CocktailsRender } from './js/cocktailsRender';
@@ -11,11 +11,12 @@ const SearchList = document.querySelector('.search__list');
 // ---------рендерим алфавитный поиск----------
 SearchList.insertAdjacentHTML('beforeend', cocktailUI.renderAlphabet());
 
-
+// ---------инициализация класса cocktailsApi----------
 const cocktailsApi = new CocktailsApi();
 
 // ---------пример вызова API----------
-cocktailsApi.getCocktailsBySymbol('1')
+
+/*cocktailsApi.getCocktailsBySymbol('1')
     .then(function (response) {
         // handle success
         console.log(response);
@@ -24,3 +25,7 @@ cocktailsApi.getCocktailsBySymbol('1')
         // handle error
         console.log(error);
     });
+*/
+
+// ---------инициализация класса localStorage----------
+const localStorage = new LocalStorage();
