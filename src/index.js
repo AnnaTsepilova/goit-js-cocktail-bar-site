0c1150590fb8ab99a6979dc6cbf4fdf723b7c140
Anna Tsepilova <virgo.tsav@gmail.com>
2022-12-11 13:33:13 +0200

    add js files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as cocktailsApi from './js/cocktailsApi';
+import * as localStorage from './js/localStorage';
+import * as favorites from './js/favorites';
+import * as themeSwitch from './js/themeSwitch';
+import * as cocktailsRender from './js/cocktailsRender';
+
+
