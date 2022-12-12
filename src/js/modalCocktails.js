79ec9381ff0c6ee4d2d373acad1d7cc1402cffd3
efirnet <efirnet@gmail.com>
2022-12-12 22:13:39 +0200

    fix

---
+++ src/js/modalCocktails.js
@@ -0,0 +1,20 @@
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  modal: document.querySelector("[data-modal]"),
+};
+
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle("backdrop--is-hidden");
+};
+
+refs.modal.addEventListener('click', onBackdropClick);
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    toggleModal();
+  }
+}
