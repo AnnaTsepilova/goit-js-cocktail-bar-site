cdf58b6e86105485d4ef5c045060f4a587abb3e5
Anna Tsepilova <virgo.tsav@gmail.com>
2022-12-12 20:37:04 +0200

    refacktor favorites

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -3,75 +3,122 @@ import { LocalStorage } from './localStorage';
 import { CocktailsApi } from './cocktailsApi';
 import { x } from './test';
 
-const locStorage = new LocalStorage();
-const coctailApi = new CocktailsApi();
+const cocktailApi = new CocktailsApi();
 
 const getId = document.querySelector('.btn-add_and_remove');
-const KEY = 'keyLocal';
-localStorage.setItem(key, JSON.stringify(x));
 
 export class ApiFavorite {
-  static BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
+  static localStorageKey = 'favorite-cocktails';
 
-  constructor() {}
+  constructor() { }
 
-  addCocktailById() {
-    const id = locStorage.saveByKey(key, idDrink);
+  addCocktailById(cocktailID) {
+    cocktailApi.getCocktailById(cocktailID).then(function (resposne) {
+      this.addCocktail(resposne);
+    });
+  }
+
+  addCocktail(cocktailObject) {
+    // Check for cocktail ID
+    if (!cocktailObject.idDrink) {
+      return;
+    }
+ 
+    let cocktails = this.getAllCocktails();
+    //console.log(cocktails);
+    if (!cocktails) {
+      cocktails = [];
+    }
+
+    // Check for added cocktail
+    let result = cocktails.filter(cocktail => cocktail.idDrink === cocktailObject.idDrink);
+    if (result.length) {
+      return;
+    }
+
+    cocktails.push(cocktailObject);
+
+    localStorage.setItem(ApiFavorite.localStorageKey, JSON.stringify(cocktails));
+  }
+
+  removeCocktailById(cocktailID) {
+    if (!cocktailID) {
+      return;
+    }
+    let cocktails = this.getAllCocktails();
+    if (!cocktails) {
+      return;
+    }
+
+    for (let key in cocktails) {
+      if (cocktails[key].idDrink == cocktailID) {
+        cocktails.splice(key, 1); 
+      }
+    }
+
+    localStorage.setItem(ApiFavorite.localStorageKey, JSON.stringify(cocktails));
+  }
+
+  getAllCocktails() {
+    return JSON.parse(localStorage.getItem(ApiFavorite.localStorageKey));
   }
 
   addIngredientByName(newIngredient) {}
 
-  removeCocktailById(removeId) {
+  /*removeCocktailById(removeId) {
     const index = this.arrCockt.indexOf(removeId);
     this.arrCockt.splice(index, 1);
-  }
+  }*/
 
   removeIngredientByName() {}
 
-  async getAllCocktails() {
+  /*async getAllCocktails() {
     const memoryId = JSON.parse(locStorage.getByKey(id));
     const arrId = memoryId.filter(el => el.idDrink);
     coctailApi.getCocktailById(arrId);
-  }
+  }*/
 
-  renderAllCocktails(data) {
-    const arr = JSON.parse(localStorage.getByKey(key));
-    return arr
-      .map(elem => {
+  renderAllCocktails() {
+    const cocktails = this.getAllCocktails();
+    if (!cocktails) {
+      return;
+    }
+    return cocktails
+      .map(cocktail => 
         `
-    <li class="list-favorite__item">
-    <img class="list-favorite_img" src="${elem}" alt="${elem}" />
-    <h3 class="list-favorite_coctail">${elem}</h3>
-    <div class="list-favorite_btn">
-      <button class="btn-learn_more">Learn more</button>
-      <button class="btn-add_and_remove">
-        Remove
-        <svg class="icon-heart__svg" width="22" height="19">
-          <use href="./images/sprite.svg#icon-heart"></use>
-        </svg>
-      </button>
-    </div>
-  </li>`;
-      })
-      .join(' ');
+        <li class="list-favorite__item" id="c_${cocktail.idDrink}">
+          <img class="list-favorite_img" src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}" />
+          <h3 class="list-favorite_coctail">${cocktail.strDrink}</h3>
+          <div class="list-favorite_btn">
+            <button class="btn-learn_more">Learn more</button>
+            <button class="btn-add_and_remove" data-cocktail-id="${cocktail.idDrink}">
+              Remove
+              <svg class="icon-heart__svg" width="22" height="19">
+                <use href="./images/sprite.svg#icon-heart"></use>
+              </svg>
+            </button>
+          </div>
+        </li>`
+      )
+      .join('');
   }
 
   getAllIngredients(data) {
     return data
       .map(elem => {
         `
-    <li class="list-ingredients__item">
-              <h3 class="list-ingredients__name">${data}</h3>
-              <p class="list-ingredients__descr">${data}</p>
-              <div class="box-btn">
-                <button class="btn-learn_more">Learn more</button>
-                <button class="btn-add_and_remove">
-                  Remove<svg class="icon-heart__svg" width="22" height="19">
-                    <use href="./images/sprite.svg#icon-heart"></use>
-                  </svg>
-                </button>
-              </div>
-            </li>`;
+        <li class="list-ingredients__item">
+          <h3 class="list-ingredients__name">${data}</h3>
+          <p class="list-ingredients__descr">${data}</p>
+          <div class="box-btn">
+            <button class="btn-learn_more">Learn more</button>
+            <button class="btn-add_and_remove">
+              Remove<svg class="icon-heart__svg" width="22" height="19">
+                <use href="./images/sprite.svg#icon-heart"></use>
+              </svg>
+            </button>
+          </div>
+        </li>`;
       })
       .join(' ');
   }
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,8 @@ export const refs = {
     searchList: document.querySelector('.search__list'),
     searchDatalist: document.querySelector('#cocktails__list'),
     searchMobileInput: document.querySelector('.search__input'),
+    cocktailsList: document.querySelector('.list-favorite'),
 
     
+   
 };
