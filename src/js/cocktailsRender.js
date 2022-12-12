5c5d5bcd387bb8e3dbc5e5a7f50ebb1758d50012
Anna Tsepilova <virgo.tsav@gmail.com>
2022-12-12 10:56:33 +0200

    refacktor mobile Alphabet

--- src/js/cocktailsRender.js
+++ src/js/cocktailsRender.js
@@ -1,3 +1,5 @@
+import { refs } from './refs';
+
 export class CocktailsRender {
   constructor() {}
   // --------генератор алфавита------------
@@ -18,24 +20,15 @@ export class CocktailsRender {
       )
       .join('');
   }
+
+  //--------генерация опций в списке алфавита моб
+  renderOptionDataList() {
+    return this.generateAlphabet().forEach(letter => {
+      let option = document.createElement('option');
+      option.value = letter;
+      refs.searchDatalist.appendChild(option);
+    });
+  }
 }
 
-// НОВОЕ (руками убрать бекспейсом букву А на сайте и выпадет список)
 
-const searchDatalist = document.querySelector('#cocktails__list');
-// ----не могу достучаться к ней из класса, надо пофиксить
-function generateAlphabet() {
-  const alphabet = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
-  const numbers = [...Array(9)].map((_, i) => String.fromCharCode(i + 49));
-  const num0 = numbers.push(String.fromCharCode(48));
-  return [...alphabet, ...numbers];
-}
-//--------генерация опций в списке алфавита моб
-function renderOptionDataList() {
-  return generateAlphabet().forEach(letter => {
-    let option = document.createElement('option');
-    option.value = letter;
-    searchDatalist.appendChild(option);
-  });
-}
-renderOptionDataList();
